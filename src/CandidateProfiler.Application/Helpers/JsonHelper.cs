008d75adc371cf12e6b6c53676249348837bb665
Mauro Gioberti <maurol.gioberti@gmail.com>
2025-12-23 02:42:05 +0100

    Refactor Json Parser

--- src/CandidateProfiler.Application/Helpers/JsonHelper.cs
+++ src/CandidateProfiler.Application/Helpers/JsonHelper.cs
@@ -6,32 +6,53 @@ public static class JsonHelper
 {
     public static JObject? TryParseJson(string content)
     {
+        var directParse = TryDirectParse(content);
+        if (directParse is not null)
+            return directParse;
+
+        return TryExtractAndParseJson(content);
+    }
+
+    private static JObject? TryDirectParse(string content)
+    {
         try
         {
             return JObject.Parse(content);
         }
         catch
         {
-            content = content.Trim();
-
-            int firstBrace = content.IndexOf('{');
-            int lastBrace = content.LastIndexOf('}');
-
-            if (firstBrace >= 0 && lastBrace > firstBrace)
-            {
-                var validJsonSubstring = content.Substring(firstBrace, lastBrace - firstBrace + 1);
-
-                try
-                {
-                    return JObject.Parse(validJsonSubstring);
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-
             return null;
         }
+    }
+
+    private static JObject? TryExtractAndParseJson(string content)
+    {
+        var trimmedContent = content.Trim();
+        var jsonBounds = FindJsonBounds(trimmedContent);
+
+        if (!jsonBounds.HasValue)
+            return null;
+
+        var (startIndex, endIndex) = jsonBounds.Value;
+        var extractedJson = ExtractJsonSubstring(trimmedContent, startIndex, endIndex);
+
+        return TryDirectParse(extractedJson);
+    }
+
+    private static (int StartIndex, int EndIndex)? FindJsonBounds(string content)
+    {
+        var firstBrace = content.IndexOf('{');
+        var lastBrace = content.LastIndexOf('}');
+
+        if (firstBrace < 0 || lastBrace <= firstBrace)
+            return null;
+
+        return (firstBrace, lastBrace);
+    }
+
+    private static string ExtractJsonSubstring(string content, int startIndex, int endIndex)
+    {
+        var length = endIndex - startIndex + 1;
+        return content.Substring(startIndex, length);
     }
 }
